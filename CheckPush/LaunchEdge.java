569791cd48b845b18000cfa33853a7882233828a
Kamran <Kamran@DELL>
2022-08-29 22:36:56 +0530

    check

---
+++ CheckPush/LaunchEdge.java
@@ -0,0 +1,19 @@
+package com.telus.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class LaunchEdge {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.edge.driver",".\\Drivers\\msedgedriver.exe");
+		WebDriver edge = new EdgeDriver();
+		String url = "https://nxtgenaiacademy.com";
+		edge.get(url);
+		
+		edge.manage().window().maximize();
+		
+
+	}
+
+}
